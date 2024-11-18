0f5b67cf6c88c1d45c73bc30ff6c043aa17edac1
David Dobrovolný <dejv.dobrovolny@seznam.cz>
2024-11-18 21:45:55 +0100

    adding connect + connection string

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 app.listen(8000, () => {
@@ -8,3 +9,14 @@ app.listen(8000, () => {
 app.get("/", (req, res) => {
   res.send("Hello API");
 });
+
+mongoose
+  .connect(
+    "mongodb+srv://dejvdobrovolny642001:ONYYiC8Desbnbn7C@wtbbtwbackend.1tdce.mongodb.net/shopping_lists?retryWrites=true&w=majority&appName=WTBBTWbackend"
+  )
+  .connect.then(() => {
+    console.log("Conected to databese.");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
