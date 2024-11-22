d2c8a21e51934f3f313d767805d48957ceb4b1e2
David Dobrovolný <dejv.dobrovolny@seznam.cz>
2024-11-22 16:05:21 +0100

    controller by list is updated

--- server/controllers/shoppingLists.js
+++ server/controllers/shoppingLists.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const CreateAbl = require("../abl/createAbl");
+const ListAbl = require("../abl/listAbl");
 
 const router = express.Router();
 
 router.post("/create", CreateAbl);
+router.get("/list", ListAbl);
 
 module.exports = router;
