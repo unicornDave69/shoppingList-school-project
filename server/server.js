878fc8ba8f0ed3e135cf4b84c1238371819e5421
David Dobrovolný <dejv.dobrovolny@seznam.cz>
2024-11-22 15:38:04 +0100

    server is updated

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const ShoppingList = require("./abl/createAbl");
+
+const ShoppingListController = require("./controllers/shoppingLists");
 
 const app = express();
 
 app.use(express.json());
 
 mongoose
-  .connect("mongodb://localhost:27017/WTBBTW")
+  .connect("mongodb://localhost:27017/WTBBTW", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("Connected to database.");
   })
@@ -15,20 +19,12 @@ mongoose
     console.log("Error connecting to database:", err);
   });
 
+app.use("/lists", ShoppingListController);
+
+app.use((req, res) => {
+  res.status(404).send("404 Not Found");
+});
+
 app.listen(8000, () => {
   console.log("Server is running on port 8000!");
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello API");
-});
-
-app.post("/list/create", async (req, res) => {
-  try {
-    const list = await ShoppingList.create(req.body);
-    res.status(201).json(list);
-  } catch (error) {
-    console.error("Error creating shopping list:", error.message);
-    res.status(500).json({ message: error.message });
-  }
 });
