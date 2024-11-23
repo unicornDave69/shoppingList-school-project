4fccecae6068939b286da4232afbf750ea50604e
David Dobrovolný <dejv.dobrovolny@seznam.cz>
2024-11-23 11:59:44 +0100

    get by id updates and it works correctly

--- server/abl/getAbl.js
+++ server/abl/getAbl.js
@@ -3,11 +3,14 @@ const ShoppingList = require("../model/ShoppingList");
 async function GetByIdAbl(req, res) {
   try {
     const { id } = req.params;
-    const getListById = await ShoppingList.findById({ id });
+    const getListById = await ShoppingList.findById(id);
+    if (!getListById) {
+      return res.status(404).json({ error: "Shopping list not found." });
+    }
     res.status(200).json(getListById);
   } catch (error) {
-    console.error("Error retrieving shopping lists:", error.stack);
-    res.status(500).json({ error: "Failed to retrieve shopping lists." });
+    console.error("Error retrieving shopping list:", error.stack);
+    res.status(500).json({ error: "Failed to retrieve shopping list." });
   }
 }
 
--- server/controllers/shoppingLists.js
+++ server/controllers/shoppingLists.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const CreateAbl = require("../abl/createAbl");
 const ListAbl = require("../abl/listAbl");
+const GetByIdAbl = require("../abl/getAbl");
 
 const router = express.Router();
 
 router.post("/create", CreateAbl);
 router.get("/list", ListAbl);
-router.get("/get/:id ", ListAbl);
+router.get("/get/:id", GetByIdAbl);
 
 module.exports = router;
