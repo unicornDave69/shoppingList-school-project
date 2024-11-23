6d2427f1424fbfbd1c507f4391d2499330797664
David Dobrovolný <dejv.dobrovolny@seznam.cz>
2024-11-23 12:09:34 +0100

    update controller

--- server/controllers/shoppingLists.js
+++ server/controllers/shoppingLists.js
@@ -2,11 +2,13 @@ const express = require("express");
 const CreateAbl = require("../abl/createAbl");
 const ListAbl = require("../abl/listAbl");
 const GetByIdAbl = require("../abl/getAbl");
+const UpdateByIdAbl = require("../abl/updateAbl");
 
 const router = express.Router();
 
 router.post("/create", CreateAbl);
 router.get("/list", ListAbl);
 router.get("/get/:id", GetByIdAbl);
+router.put("/put/:id", UpdateByIdAbl);
 
 module.exports = router;
