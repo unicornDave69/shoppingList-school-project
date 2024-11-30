a940d0fb2f7ed39459642174ab7f9b552c6e25a4
David Dobrovolný <dejv.dobrovolny@seznam.cz>
2024-11-30 21:43:30 +0100

    update contoller by middleware

--- server/controllers/shoppingLists.js
+++ server/controllers/shoppingLists.js
@@ -4,13 +4,14 @@ const ListAbl = require("../abl/listAbl");
 const GetByIdAbl = require("../abl/getAbl");
 const UpdateByIdAbl = require("../abl/updateAbl");
 const DeleteAbl = require("../abl/deleteAbl");
+const authenticateJWT = require("../middleware/middleware");
 
 const router = express.Router();
 
-router.post("/create", CreateAbl);
-router.get("/list", ListAbl);
-router.get("/get/:id", GetByIdAbl);
-router.put("/put/:id", UpdateByIdAbl);
-router.delete("/delete", DeleteAbl);
+router.post("/create", authenticateJWT, CreateAbl);
+router.get("/list", authenticateJWT, ListAbl);
+router.get("/get/:id", authenticateJWT, GetByIdAbl);
+router.put("/put/:id", authenticateJWT, UpdateByIdAbl);
+router.delete("/delete", authenticateJWT, DeleteAbl);
 
 module.exports = router;
