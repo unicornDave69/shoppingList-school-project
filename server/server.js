ec23addbdefb9c39a3b01cf08acc3bbf77bc3537
David Dobrovolný <dejv.dobrovolny@seznam.cz>
2024-12-03 22:10:22 +0100

    update serverjs + cors

--- server/server.js
+++ server/server.js
@@ -1,37 +1,29 @@
+const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
-
 require("dotenv").config();
-app.use(cors());
-
-const ShoppingListController = require("./controllers/shoppingLists");
-const AuthController = require("./abl/auth/regLog-userAbl");
 
 const app = express();
 
+// cors configuration
+const corsOptions = {
+  origin: "http://localhost:3000", //allow only front-end na localhost:3000
+  methods: ["GET", "POST", "PUT", "DELETE"], // HTTP metods
+  allowedHeaders: ["Content-Type"],
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
-const MONGO_URI =
-  process.env.MONGO_URI || "mongodb://localhost:27017/WTBBTW/shoppinglists";
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/WTBBTW";
 mongoose
   .connect(MONGO_URI)
-  .then(() => {
-    console.log("Connected to database.");
-  })
-  .catch((err) => {
-    console.log("Error connecting to database:", err);
-  });
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
-//app.use("/auth", AuthController);
-
-app.use("/", ShoppingListController);
-
-app.use((req, res) => {
-  res.status(404).send("404 Not Found");
-});
+// controller import
+const ShoppingListController = require("./controllers/shoppingLists");
+app.use("/api/lists", ShoppingListController);
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}!`);
-});
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
