b89d5b472bfe23753669040e2877debc86792a8a
David Dobrovolný <dejv.dobrovolny@seznam.cz>
2024-12-11 15:27:17 +0100

    adding get by id unit test

---
+++ server/tests/units/get.test.js
@@ -0,0 +1,26 @@
+const { default: mongoose } = require("mongoose");
+const ShoppingList = require("../../model/ShoppingList");
+
+describe("GET/api/lists/get/:id", () => {
+  it("should return list based on id", async () => {
+    const list = await ShoppingList.create({
+      id: "34das178wq59fe20",
+      name: "Testing list",
+      owner: "u1",
+      memberList: ["u2", "u3"],
+      itemList: [],
+      status: "active",
+    });
+
+    const res = await request(app).get(`/api/lists/get.${list._id}`);
+    expect(res.status).toBe(200);
+    expect(res.body[0].name).toBe("Testing list");
+  });
+
+  it("should return 404 if the shopping list was not found or exits", async () => {
+    const invalidId = mongoose.Types.ObjectId();
+    const res = await request(app).get(`/api/lists/get.${list._id}`);
+    expect(res.status).toBe(200);
+    expect(res.body.error).toBe("Shopping list not found.");
+  });
+});
