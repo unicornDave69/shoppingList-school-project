5765614e6c88087785b1b0b12550703368cb7edf
David Dobrovolný <dejv.dobrovolny@seznam.cz>
2024-12-13 16:43:51 +0100

    listing data from databaze, not from provider

--- server/server.js
+++ server/server.js
@@ -17,6 +17,19 @@ app.use(express.json());
 
 const MONGO_URI = "mongodb://localhost:27017/WTBBTW";
 
+// Přidání obsluhy událostí připojení
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose is connected to MongoDB");
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose is disconnected from MongoDB");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error("Mongoose connection error:", err);
+});
+
 const connectToMongoDB = async () => {
   try {
     await mongoose.connect(MONGO_URI);
@@ -26,11 +39,19 @@ const connectToMongoDB = async () => {
   }
 };
 
+// Připojení k databázi
 connectToMongoDB();
 
 // Import controlleru
 const ShoppingListController = require("./controllers/shoppingLists");
 app.use("/api/lists", ShoppingListController);
+
+// V případě ukončení procesu aplikace
+process.on("SIGINT", async () => {
+  console.log("Application is shutting down...");
+  await mongoose.disconnect();
+  process.exit(0);
+});
 
 if (process.env.NODE_ENV !== "test") {
   app.listen(8005, () => {
